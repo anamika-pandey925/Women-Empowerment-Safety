ff7ca9f323120bca58873f99b291084bd33357d4
Anamika Pandey <126402435+anamika-pandey925@users.noreply.github.com>
2026-06-10 14:21:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Women-Empowerment-Safety/',
+
   plugins: [
     react(),
-    // VitePWA({...}) 
-    // Temporarily disabled to debug CSP issue
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   server: {
     proxy: {
       '/api': {
